d0e00d90416e63b17ccf2786b6e430ac436e6d52
anermers <gaklifeZ@gmail.com>
2018-06-04 09:45:57 +0800

    Initial commit

---
+++ VRTEST/Assets/360STUFF/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    Mesh mesh;
+	// Use this for initialization
+	void Start () {
+        mesh = GetComponent<MeshFilter>().mesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
