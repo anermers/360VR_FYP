e8abe4c1eb6f4435fcc3cde328254f6daa2d1def
Samuel <bluecubestuff@gmail.com>
2018-06-04 17:57:31 +0800

    Meshmorphing, test images

---
+++ VRTest1/Assets/Scripts/SkyboxTextureHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SkyboxTextureInfo
+{
+    public string key;
+    public Texture value;
+}
+
+public class SkyboxTextureHandler : MonoBehaviour
+{
+
+    public static SkyboxTextureHandler instance = null;
+    public Material skyboxMaterial;
+    public List<SkyboxTextureInfo> textureInfoList; 
+    private Dictionary<string, Texture> textureContainer;
+
+    private void Awake()
+    {
+        if (!instance)
+            instance = this;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        textureContainer = new Dictionary<string, Texture>();
+        
+        // Adds item in list to the dictionary
+        foreach (SkyboxTextureInfo info in textureInfoList)
+        {
+            if (textureContainer.ContainsKey(info.key))
+                continue;
+
+            textureContainer.Add(info.key, info.value);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+
+    bool ChangeTexture(string key)
+    {
+        if(!textureContainer.ContainsKey(key))
+            return false;
+
+        // Set skybox texture 
+        skyboxMaterial.SetTexture("_Tex", textureContainer[key]);
+        return true;
+    }
+
+
+}
