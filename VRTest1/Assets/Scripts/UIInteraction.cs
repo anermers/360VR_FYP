210f68e303513943be7ea43416fd10484ec262d8
anermers <gaklifeZ@gmail.com>
2018-06-05 10:59:15 +0800

    VR kitchen button/ VRkitchen mode

--- VRTest1/Assets/Scripts/UIInteraction.cs
+++ VRTest1/Assets/Scripts/UIInteraction.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UIInteraction : MonoBehaviour {
     public UnityEngine.UI.Text outText;
+    public GameObject kitchenModel;
 
 	public void OnButtonClicked() {
-        if (outText != null) {
+        if (kitchenModel != null)
+            if (EventSystem.current.currentSelectedGameObject.name == "VRKitchenButton")
+                    kitchenModel.SetActive(!kitchenModel.activeInHierarchy);
+
+        else if (outText != null) {
             outText.text = "UI Button clicked";
         }
     }
